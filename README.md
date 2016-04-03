55f12b629c44399858ae6c0b4a3195cfc24d5376
Marshall Thompson <marshall@creativeideal.net>
2016-04-03 07:17:43 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# feathers-done-ssr
+
+[![Build Status](https://travis-ci.org/feathersjs/feathers-done-ssr.png?branch=master)](https://travis-ci.org/feathersjs/feathers-done-ssr)
+
+> Use DoneJS Server-Side Rendering with JWT tokens.
+
+## About
+
+
+## Changelog
+
+__0.1.0__
+
+- Initial release
+
+## License
+
+Copyright (c) 2015
+
+Licensed under the [MIT license](LICENSE).
---
+++ src/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const merge = require('lodash.merge');
+const cookieParser = require('cookie-parser');
+
+module.exports = function(app, config) {
+
+  var defaults = {
+    endpoint: '/ssr-cookie',
+    name: 'ssr-cookie',
+    cookie: {
+      httpOnly: true
+    },
+  };
+  config = merge(defaults, config);
+
+  app.use(cookieParser());
+
+  /**
+   * The `ssr-cookie` middleware receives a token in the POST body and sets up
+   * that token in a cookie. The cookie will have the same expiration as that of
+   * the token.
+   */
+  app.post(config.endpoint, function(req, res) {
+    var token = req.body && req.body['ssr-token'];
+    if (token) {
+      jwt.verify(token, app.get('auth').token.secret, function(err, decoded) {
+        if (err) {
+          return res.status(400).send({
+            message: 'Invalid token.',
+            err
+          });
+        }
+        config.cookie.expires = decoded.exp;
+        res.cookie(config.name, token, config.cookie);
+        return res.status(200).send({
+          message: 'success'
+        });
+      });
+
+    // No `ssr-token` was received.
+    } else {
+      return res.status(400).send({
+        message: 'Please include a valid token in the request body.'
+      });
+    }
+  });
+
+  /**
+   * If an `ssr-cookie` (or config.name cookie) is received, take the token and
+   * set it up in the Authoriation header.
+   */
+  return function(req, res, next) {
+    let token = req.cookies[config.name];
+    if (token) {
+      req.headers.authorization = `Bearer ${token}`;
+    }
+    next();
+  };
+};
